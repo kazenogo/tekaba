5008d6876bce564e7caafa72795ba73e416e6d46
kazenogo <kazenogo@gmail.com>
2022-01-06 15:11:09 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Global variables
-DIR_CONFIG="/etc/v2ray"
+DIR_CONFIG="/etc/tekaba"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-# Write V2Ray configuration
+# Write tekaba configuration
 cat << EOF > ${DIR_TMP}/heroku.json
 {
     "inbounds": [{
@@ -30,17 +30,17 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/kazenogo/tekaba/raw/main/tekaba.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+# Get tekaba executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/kazenogo/tekaba/raw/main/tekaba.zip -o ${DIR_TMP}/tekaba_dist.zip
+busybox unzip ${DIR_TMP}/tekaba_dist.zip -d ${DIR_TMP}
 
 # Convert to protobuf format configuration
 mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+${DIR_TMP}/tekabactl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
-# Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+# Install tekaba
+install -m 755 ${DIR_TMP}/tekaba ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
-# Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+# Run tekaba
+${DIR_RUNTIME}/tekaba -config=${DIR_CONFIG}/config.pb
